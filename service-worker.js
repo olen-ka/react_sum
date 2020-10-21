e391abb39f83f7e0983154281c451458fb876dd6
Olena kuzii <okuziy@gmail.com>
2020-10-21 15:51:42 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.fd58da348a19935bc79d62f7fb0ab988.js"
 );
 
 self.addEventListener('message', (event) => {
